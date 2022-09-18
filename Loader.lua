22373161cb8eb5cd126939cf04e667f2af631980
SippyHUB <36508239+sippyhub@users.noreply.github.com>
2022-09-19 03:21:41 +0700

    Delete Loader.lua

--- Loader.lua
+++
@@ -1,7 +0,0 @@
-local key = _G.Key
-local check = "https://www.sippyhub.xyz/scripts/check.php?key=" .. key
-if game:HttpGet(check) == "Whitelisted" then
-loadstring(game:HttpGet("https://www.sippyhub.xyz/scripts/script.lua"))()
-else
-game.Players.LocalPlayer:Kick("Invalid Key! Please Rejoin And Try Again.")
-end
